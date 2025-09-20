df549ef32102045d29e75f47f3bd9b2de71020c7
Gino Gregoret <gregoretgino@gmail.com>
2025-09-19 22:07:02 -0300

    movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import { useEffect, useState } from 'react'
 import {Contenedor} from './components/Contenedor'
 import {Tarjeta} from './components/Tarjeta'
+
 function App() {
+
+  useEffect(()=>{
+    const url = import.meta.env.VITE_URL
+    const token = import.meta.env.VITE_TOKEN
+    const config={
+      method:"GET",
+      Headers: {
+        'content-type': "application/json",
+        authorization: `Bearer ${token}`
+      },
+    }
+    fetch(url, config)
+    .then((data)=>data.json())
+    .then((data)=> console.log(data))
+  },[])
+  
+  
   return (
     <div>
       Contenedor
